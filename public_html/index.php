1b5dbaeee72c83819b4c51f9b9cabe57c3d0083a
Jacob Shirley <jakeshirley2@gmail.com>
2018-03-09 18:56:59 +0000

    First commit.

---
+++ public_html/index.php
@@ -0,0 +1,49 @@
+<?php
+use \Twig\Loader\Filesystem;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once '../vendor/autoload.php';
+
+$loader = new Twig_Loader_Filesystem('assets/templates/');
+
+$twig = new Twig_Environment($loader);
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$c = new \Slim\Container($configuration);
+$app = new \Slim\App($c);
+
+$app->get('/', function (Request $request, Response $response) {
+    global $twig;
+    $response->getBody()->write($twig->render("index.html"));
+
+    return $response;
+});
+
+$app->get('/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+
+	global $twig;
+    $response->getBody()->write($twig->render($name . ".html"));
+
+    return $response;
+});
+
+$app->get('/api/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+
+	global $twig;
+    $template = $twig->load($name . ".html");
+
+    $response->getBody()->write($template->render(array("api" => true)));
+
+    return $response;
+});
+
+$app->run();
