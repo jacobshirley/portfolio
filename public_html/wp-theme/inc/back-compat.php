d8588d59b2c36f3e9d441654f33c7288a87bb7ef
Jacob Shirley <auriferous95@gmail.com>
2018-04-01 18:08:08 +0100

    Theme in progress.

---
+++ public_html/wp-theme/inc/back-compat.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Jacob Shirley back compat functionality
+ *
+ * Prevents Jacob Shirley from running on WordPress versions prior to 4.4,
+ * since this theme is not meant to be backward compatible beyond that and
+ * relies on many newer functions and markup changes introduced in 4.4.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Jacob Shirley 1.0
+ */
+
+/**
+ * Prevent switching to Jacob Shirley on old versions of WordPress.
+ *
+ * Switches to the default theme.
+ *
+ * @since Jacob Shirley 1.0
+ */
+function jacobshirley_switch_theme() {
+	switch_theme( WP_DEFAULT_THEME, WP_DEFAULT_THEME );
+
+	unset( $_GET['activated'] );
+
+	add_action( 'admin_notices', 'jacobshirley_upgrade_notice' );
+}
+add_action( 'after_switch_theme', 'jacobshirley_switch_theme' );
+
+/**
+ * Adds a message for unsuccessful theme switch.
+ *
+ * Prints an update nag after an unsuccessful attempt to switch to
+ * Jacob Shirley on WordPress versions prior to 4.4.
+ *
+ * @since Jacob Shirley 1.0
+ *
+ * @global string $wp_version WordPress version.
+ */
+function jacobshirley_upgrade_notice() {
+	$message = sprintf( __( 'Jacob Shirley requires at least WordPress version 4.4. You are running version %s. Please upgrade and try again.', 'jacobshirley' ), $GLOBALS['wp_version'] );
+	printf( '<div class="error"><p>%s</p></div>', $message );
+}
+
+/**
+ * Prevents the Customizer from being loaded on WordPress versions prior to 4.4.
+ *
+ * @since Jacob Shirley 1.0
+ *
+ * @global string $wp_version WordPress version.
+ */
+function jacobshirley_customize() {
+	wp_die( sprintf( __( 'Jacob Shirley requires at least WordPress version 4.4. You are running version %s. Please upgrade and try again.', 'jacobshirley' ), $GLOBALS['wp_version'] ), '', array(
+		'back_link' => true,
+	) );
+}
+add_action( 'load-customize.php', 'jacobshirley_customize' );
+
+/**
+ * Prevents the Theme Preview from being loaded on WordPress versions prior to 4.4.
+ *
+ * @since Jacob Shirley 1.0
+ *
+ * @global string $wp_version WordPress version.
+ */
+function jacobshirley_preview() {
+	if ( isset( $_GET['preview'] ) ) {
+		wp_die( sprintf( __( 'Jacob Shirley requires at least WordPress version 4.4. You are running version %s. Please upgrade and try again.', 'jacobshirley' ), $GLOBALS['wp_version'] ) );
+	}
+}
+add_action( 'template_redirect', 'jacobshirley_preview' );
